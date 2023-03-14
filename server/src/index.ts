861e497f2cf76f1e5b39723cef2ee9959756abe9
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-14 18:56:24 +0530

    Fixed some dependencies issues

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import config from "config";
 
 const app = express();
 
+const PORT = config.get<string>("PORT");
+
+console.log(PORT);
