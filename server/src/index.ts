52fbffbb856fa41a0deacb2b71b4e8864d2cb3a2
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-14 19:05:52 +0530

    Setup Environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import config from "config";
+import logger from "./utils/logger";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
 
-const PORT = config.get<string>("PORT");
+const PORT = config.get<string>("PORT") || 4000;
 
-console.log(PORT);
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: "*",
+  })
+);
+
+app.get("/health-check", (req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.OK).send({ message: "OK" });
+  next();
+});
+
+const server = app.listen(PORT, () => {
+  logger.info(`The server is running at http://localhost:${PORT}`);
+});
